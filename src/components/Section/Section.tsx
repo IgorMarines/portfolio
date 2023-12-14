cddc19bea50fc373013c3e2ef509c73f4fd3b86e
IgorMarines <igor.marines25@gmail.com>
2023-12-14 01:33:22 -0300

    implementação do botão de contato

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -11,12 +11,13 @@ const Content = () => {
     <S.Body>
       <S.Border>
         <S.Container className='animate__slideInDown'>
-          <S.Left style={{width: isMobile ? '90%' : '30%'}}>
+          <S.Left style={{ width: isMobile ? '90%' : '30%' }}>
             <S.Name className="animate__animated animate__fadeIn animate__delay-1s">Oi,</S.Name>
             <S.Role className="animate__animated animate__fadeIn animate__delay-2s">Sou o <a href='/about'>Igor Marines</a></S.Role>
-            <S.Description className="animate__animated animate__fadeIn animate__delay-3s">Eu escrevo códigos com simplicidade e estilo.</S.Description>
+            <S.Description className="animate__animated animate__fadeIn animate__delay-3s">Eu escrevo códigos com simplicidade e estilo.
+              <a style={{ color: '#000', display: 'block', background: '#fff', textDecoration: 'none', width: 200, borderRadius: 5, marginTop: 15, padding: '5px 5px', textAlign: 'center' }} href="https://wa.me/5511954348405" target='_blank'> Fale Comigo</a></S.Description>
           </S.Left>
-          <S.Right style={{display: isMobile ? 'none' : 'flex'}}>
+          <S.Right style={{ display: isMobile ? 'none' : 'flex' }}>
             <img className="animate__animated animate__fadeIn animate__delay-4s" src={ena} alt="" />
           </S.Right>
         </S.Container>
